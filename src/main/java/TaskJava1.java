970561cf90b6ae5bd815a8c6afc1287042d3d15b
Einzel <einzel0102@mail.ru>
2023-04-20 00:41:01 +0800

    "Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, используя StringBuilder. Если значение null, то параметр не должен попадать в запрос."

--- src/main/java/TaskJava1.java
+++ src/main/java/TaskJava1.java
@@ -13,33 +13,37 @@
  */
 
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 
 public class TaskJava1 {
     public static void main(String[] args) {
-        Map<String, String> params1 = new HashMap<String,String>(); // создаем словарь и заносим значения в него
-        params1.put("name","Petr");
-        params1.put("country","Russia");
-        params1.put("city","Moskow");
-        params1.put("age",null);
-        System.out.println(getQuery(params1));
-    }
-    public static String getQuery(Map<String, String> params)
-    {
-        StringBuilder s = new StringBuilder();
-        for (Map.Entry<String,String> pair : params.entrySet()) // указываем тип значения в <String,String>(можно менять)
-        {
-            if (pair.getValue() != null)
-            {
-                s.append(pair.getKey() +" = '" + pair.getValue()+"' and ");
+        String stringToFilter = "\"name:Ivanov\" \"country:Russia\" \"city:Moscow\" \"age:null\"";
+        String[] stringToFilterArrayNew = stringToFilter.split("\"?\\s+\"?|:\"?");
+        System.out.println(Arrays.toString(stringToFilterArrayNew));
+        //if (stringToFilterArrayNew.length > 0) { // проверяем, что массив не пустой
+            //String[] keyValuePairs = stringToFilterArrayNew[0].split(",");
+        StringBuilder filterBuilder = new StringBuilder();
+
+        for (int i = 0; i < stringToFilterArrayNew.length + 1; i += 2) {
+            if (i + 1 < stringToFilterArrayNew.length) { // проверяем, что есть следующий элемент
+                String key = stringToFilterArrayNew[i];
+                String value = stringToFilterArrayNew[i + 1];
+                if (!value.equals("null\"")) {
+                    if (filterBuilder.length() > 0) {
+                            filterBuilder.append(" and ");
+                    }
+                    filterBuilder.append(key).append(" = '").append(value).append("'");
+                }
             }
         }
-        s.delete(s.toString().length()-5,s.toString().length());
-        return s.toString();
-    }
 
+        String filter = filterBuilder.toString();
+        System.out.println(filter);
+        //} else {
+           //System.out.println("Массив пустой");
+        //}
+    }
 }
 
 
