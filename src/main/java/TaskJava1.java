2149b9d9a445091bc4fa14cd6b88be4689fc9bf5
Einzel <einzel0102@mail.ru>
2023-04-20 09:31:11 +0800

    "Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, используя StringBuilder. Если значение null, то параметр не должен попадать в запрос."

--- src/main/java/TaskJava1.java
+++ src/main/java/TaskJava1.java
@@ -18,7 +18,7 @@ import java.util.Arrays;
 
 public class TaskJava1 {
     public static void main(String[] args) {
-        String stringToFilter = "\"name:Ivanov\" \"country:Russia\" \"city:Moscow\" \"age:null\"";
+        String stringToFilter = "\"name:Ivanov\" \"country:Russia\" \"city:null\" \"age:null\"";
         String[] stringToFilterArrayNew = stringToFilter.split("\"?\\s+\"?|:\"?");
         System.out.println(Arrays.toString(stringToFilterArrayNew));
         //if (stringToFilterArrayNew.length > 0) { // проверяем, что массив не пустой
@@ -29,7 +29,7 @@ public class TaskJava1 {
             if (i + 1 < stringToFilterArrayNew.length) { // проверяем, что есть следующий элемент
                 String key = stringToFilterArrayNew[i];
                 String value = stringToFilterArrayNew[i + 1];
-                if (!value.equals("null\"")) {
+                if (!value.equals("null\"") && !value.equals("null")) {
                     if (filterBuilder.length() > 0) {
                             filterBuilder.append(" and ");
                     }
